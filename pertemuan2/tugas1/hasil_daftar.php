746c7b8fc8b2eed61a36cec1e9102105201a9ef6
RiaFitriyani <114563770+RiaFitriyani@users.noreply.github.com>
2023-03-07 20:55:02 +0700

    Add files via upload

---
+++ pertemuan2/tugas1/hasil_daftar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Pendaftar</title>
+	<!-- Load Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h2>Data Pendaftar</h2>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama Lengkap</th>
+					<th>Email</th>
+					<th>Alamat</th>
+					<th>Telepon</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1.</td>
+					<td>meng</td>
+					<td>Alessandro086@gmail.com</td>
+					<td>jl.rumbut</td>
+					<td>+628971639843</td>
+				</tr>
+				<tr>
+					<td>2.</td>
+					<td>ming</td>
+					<td>riafitriyani0090@gmail.com</td>
+					<td>jl.sukatani</td>
+					<td>+6289606960407</td>
+				</tr>
+
+				<?php
+				  /* Task 2
+				    Tangkap dan tampilkan data dari form_daftar.
+						Berikan error handling
+				  */
+				?>
+
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
